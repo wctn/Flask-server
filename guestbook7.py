b8271f0fcc5838a7f99dddfafc2ff079e64d4f63
INE-01 <INE-01@Admins-iMac-5.local>
2020-07-29 19:14:16 +0700

    rfger

--- guestbook7.py
+++ guestbook7.py
@@ -27,4 +27,5 @@ def process():
     db.session.commit()
     return redirect(url_for('index'))
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.debug = True
+    app.run(host='0.0.0.0', port=80)
